8109b4445ebb475ab215b52e5f8045360081afef
Yuan <yuan.zong.man@gmail.com>
2022-06-05 16:48:17 +0200

    vehicle api test

--- P02-VehiclesAPI/vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
+++ P02-VehiclesAPI/vehicles-api/src/test/java/com/udacity/vehicles/api/CarControllerTest.java
@@ -91,11 +91,36 @@ public class CarControllerTest {
      */
     @Test
     public void listCars() throws Exception {
-        /**
-         * TODO: Add a test to check that the `get` method works by calling
-         *   the whole list of vehicles. This should utilize the car from `getCar()`
-         *   below (the vehicle will be the first in the list).
-         */
+        var expectedJson = "{\"_embedded\":{\"carList\":[{\n" +
+                "   \"condition\":\"USED\",\n" +
+                "   \"details\":{\n" +
+                "      \"body\":\"sedan\",\n" +
+                "      \"model\":\"Impala\",\n" +
+                "      \"manufacturer\":{\n" +
+                "         \"code\":101,\n" +
+                "         \"name\":\"Chevrolet\"\n" +
+                "      },\n" +
+                "      \"numberOfDoors\":4,\n" +
+                "      \"fuelType\":\"Gasoline\",\n" +
+                "      \"engine\":\"3.6L V6\",\n" +
+                "      \"mileage\":32280,\n" +
+                "      \"modelYear\":2018,\n" +
+                "      \"productionYear\":2018,\n" +
+                "      \"externalColor\":\"white\"\n" +
+                "   },\n" +
+                "   \"location\":{\n" +
+                "      \"lat\":40.73061,\n" +
+                "      \"lon\":-73.935242\n" +
+                "   }\n" +
+                "}]}}";
+
+
+        mvc.perform(
+                        get(new URI("/cars"))
+                                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                                .accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedJson));
 
     }
 
@@ -105,10 +130,36 @@ public class CarControllerTest {
      */
     @Test
     public void findCar() throws Exception {
-        /**
-         * TODO: Add a test to check that the `get` method works by calling
-         *   a vehicle by ID. This should utilize the car from `getCar()` below.
-         */
+        var expectedJson = "{\n" +
+                "   \"condition\":\"USED\",\n" +
+                "   \"details\":{\n" +
+                "      \"body\":\"sedan\",\n" +
+                "      \"model\":\"Impala\",\n" +
+                "      \"manufacturer\":{\n" +
+                "         \"code\":101,\n" +
+                "         \"name\":\"Chevrolet\"\n" +
+                "      },\n" +
+                "      \"numberOfDoors\":4,\n" +
+                "      \"fuelType\":\"Gasoline\",\n" +
+                "      \"engine\":\"3.6L V6\",\n" +
+                "      \"mileage\":32280,\n" +
+                "      \"modelYear\":2018,\n" +
+                "      \"productionYear\":2018,\n" +
+                "      \"externalColor\":\"white\"\n" +
+                "   },\n" +
+                "   \"location\":{\n" +
+                "      \"lat\":40.73061,\n" +
+                "      \"lon\":-73.935242\n" +
+                "   }\n" +
+                "}";
+
+
+        mvc.perform(
+                        get(new URI("/cars/1"))
+                                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                                .accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedJson));
     }
 
     /**
@@ -117,11 +168,11 @@ public class CarControllerTest {
      */
     @Test
     public void deleteCar() throws Exception {
-        /**
-         * TODO: Add a test to check whether a vehicle is appropriately deleted
-         *   when the `delete` method is called from the Car Controller. This
-         *   should utilize the car from `getCar()` below.
-         */
+        mvc.perform(
+                        delete(new URI("/cars/1"))
+                                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                                .accept(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(status().isNoContent());
     }
 
     /**
